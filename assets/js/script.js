1b08896fd5485d0c7d648e6267dd153f5738d7a0
Cassidy Pagels <rozeneclipse@gmail.com>
2024-10-26 21:39:58 -0500

    ID elements, define elements and API key in script

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// define page elements
+const cityInput = document.getElementById('cityInput')
+const submitBtn = document.getElementById('cityInputBtn')
+const searchHistory = document.getElementById('searchHistory')
+const currentWeatherCard = document.getElementById('currentWeather')
+const forecastField = document.getElementById('forecastField')
+
+// define API key
+const apiKey = '2301593fc1c038267904bbec75062ee9'
+
